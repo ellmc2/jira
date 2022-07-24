c50d1bfd49db40c9df5effb02dd6656090c493d0
ellmc2 <jellmc2@qq.com>
2022-07-24 17:24:55 +0800

    feat: ✨ 用Grid和Flexbox布局优化项目列表页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,12 +1,44 @@
 import { ProjectListScreen } from "./screens/project-list";
 import { useAuth } from "./context/auth-context";
+import styled from "@emotion/styled";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
+      <PageHeader>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </PageHeader>
+      <Main>
+        <ProjectListScreen />
+      </Main>
     </div>
   );
 };
+
+const PageHeader = styled.header`
+  height: 6rem;
+  background-color: gray;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  height: calc(100vh -6rem);
+`;
