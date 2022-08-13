d09d7d671607933629ab25cbb58592b595364400
ellmc2 <jellmc2@qq.com>
2022-08-13 15:20:06 +0800

    feat: ✨添加error-boundary

---
+++ src/components/error-boundary.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement, ReactNode } from "react";
+
+// children
+// fallbackRender
+type FallbackRender = (props: { error: Error | null }) => ReactElement;
+export class ErrorBoundary extends React.Component<
+  React.PropsWithChildren<{ fallbackRender: FallbackRender }>,
+  {
+    error: Error | null;
+  }
+> {
+  state = {
+    error: null,
+  };
+
+  // 当子组件抛出异常，这里会接收到并且调用
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { fallbackRender, children } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+
+    return children;
+  }
+}
